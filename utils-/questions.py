14cae8bf7be58f135b0914f18131be2a5b5fbd0e
SAHIL ANSARI <sahilansari.6137@gmail.com>
2026-07-26 00:59:20 +0530

    Create questions.py

---
+++ utils-/questions.py
@@ -0,0 +1,68 @@
+# Interview Questions
+
+QUESTIONS = {
+    "Python Developer": [
+        "Tell me about yourself.",
+        "What is Python?",
+        "Difference between List and Tuple?",
+        "What are Functions?",
+        "Explain OOP.",
+        "What is Exception Handling?",
+        "What is a Dictionary?",
+        "Explain Modules.",
+        "What is a Lambda Function?",
+        "Why should we hire you?"
+    ],
+
+    "Java Developer": [
+        "Tell me about yourself.",
+        "What is Java?",
+        "Explain JVM.",
+        "What is OOP?",
+        "Difference between JDK and JRE?",
+        "Explain Exception Handling.",
+        "What is Multithreading?",
+        "What is Inheritance?",
+        "Explain Collection Framework.",
+        "Why should we hire you?"
+    ],
+
+    "Data Analyst": [
+        "Tell me about yourself.",
+        "What is Data Analysis?",
+        "Explain Excel.",
+        "What is SQL?",
+        "Difference between INNER and LEFT JOIN?",
+        "What is Power BI?",
+        "Explain Pandas.",
+        "What is Data Cleaning?",
+        "What is Visualization?",
+        "Why should we hire you?"
+    ],
+
+    "AI/ML Engineer": [
+        "Tell me about yourself.",
+        "What is Machine Learning?",
+        "Difference between AI and ML?",
+        "Explain Supervised Learning.",
+        "What is Deep Learning?",
+        "What is Overfitting?",
+        "Explain Regression.",
+        "Explain Classification.",
+        "What is Python used for?",
+        "Why should we hire you?"
+    ],
+
+    "Web Developer": [
+        "Tell me about yourself.",
+        "What is HTML?",
+        "Difference between HTML and CSS?",
+        "Explain JavaScript.",
+        "What is Bootstrap?",
+        "What is React?",
+        "Explain API.",
+        "What is Responsive Design?",
+        "What is Git?",
+        "Why should we hire you?"
+    ]
+}
